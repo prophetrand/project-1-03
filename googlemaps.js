64d1fea27b03d76fc29b27a6348e1cdcd29dfd3d
Rand Hale <randluca@gmail.com>
2021-01-08 12:10:00 -0800

    begin developing Google Maps API insertion to the webpage. Create test.css to test styling of the map division

---
+++ googlemaps.js
@@ -0,0 +1,23 @@
+// My GoogleMaps API Key: AIzaSyB7Ma6MevHTXC2RnuetQCcPM7LUnKvyeKA
+
+// Initialize and add the map
+$(document).ready(function(){
+
+
+
+    function initMap() {
+        // The location of the continental United States, 39.82925222683178, -98.5790934619165
+        const country = { lat: 39.829, lng: -98.579 };
+        // The map, centered on USA.
+        const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: country,
+        });
+        // The marker, positioned at central USA.
+        const marker = new google.maps.Marker({
+        position: country,
+        map: map,
+        });
+    }
+initMap();
+});
