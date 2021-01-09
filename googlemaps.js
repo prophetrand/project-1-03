8dc836eb3ffc04198a60b52c08d82ecf65c5668d
Rand Hale <randluca@gmail.com>
2021-01-09 14:19:37 -0800

    write function framework for placing a pin at mouse click location. Need to fix error(s) next

--- googlemaps.js
+++ googlemaps.js
@@ -21,6 +21,10 @@ $(document).ready(function(){
     $("#map").on('mouseup mousemove', function handler(event) {
       if (event.type === 'mouseup') {
         console.log("Oh Joy");
+        const marker = new google.maps.Marker({
+            position: event.latLng, 
+            map: map
+        });
       } else {
         // drag
       }
@@ -30,3 +34,12 @@ $(document).ready(function(){
 
   initMap();
 });
+
+// https://developers.google.com/maps/documentation/javascript/examples/geocoding-simple
+// Above link will be useful in making the Map pan to the location specified by the user in the text field. This may also serve as a soft-validation by moving the pin to the typed location, such that the typed location and the pin are not in conflict.
+
+// https://forums.asp.net/t/1942206.aspx?how+to+restrict+the+map+to+a+country+using+google+api+ 
+// Try to use one of the solutions above to restrict the map bounds to the continental United States.
+
+// https://stackoverflow.com/questions/15792655/add-marker-to-google-map-on-click
+// This one is for placing a pin at the clicked location's lat/lon
