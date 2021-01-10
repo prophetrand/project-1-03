1940dc71e3da88fa3aad184b90981375592ed84a
Rand Hale <randluca@gmail.com>
2021-01-09 20:16:15 -0800

    declare a variable for map on the global scope to be accessible by all functions

--- googlemaps.js
+++ googlemaps.js
@@ -1,14 +1,17 @@
 // My GoogleMaps API Key: AIzaSyB7Ma6MevHTXC2RnuetQCcPM7LUnKvyeKA
 
 $(document).ready(function(){
-// Function to create the interactive map and append it to the division with the ID of "map." This function is called by default when the page loads, at the bottom of this script.   
+  // Declaring map on the global scope so that it is not restricted to the local scope of initMap().
+  var map;
+
+  // Function to create the interactive map and append it to the division with the ID of "map." This function is called by default when the page loads, at the bottom of this script.     
   function initMap() {
     // The location of the continental United States, 39.82925222683178, -98.5790934619165
     const country = { lat: 38.729, lng: -96.879 };
     // The map, centered on USA.
     const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4.1,
-    center: country,
+      zoom: 4.1,
+      center: country,
     });
     // The marker, positioned at central USA.
     // const marker = new google.maps.Marker({
@@ -21,16 +24,29 @@ $(document).ready(function(){
     $("#map").on('mouseup mousemove', function handler(event) {
       if (event.type === 'mouseup') {
         console.log("Oh Joy");
+        // here is the tricky bit
         const marker = new google.maps.Marker({
             position: event.latLng, 
             map: map
         });
       } else {
-        // drag
+        // When the map is dragged but not explicitly clicked at one location, nothing happens with respect to this event listener.
       }
       $("#map").off('mouseup mousemove', handler);
     });
   });
+
+  // map.addListener('click', function(event) {
+  //   placeMarker(event.latLng, map);
+  // });
+
+  // function placeMarker(position, map) {
+  //   var marker = new google.maps.Marker({
+  //       position: position,
+  //       map: map
+  //   });
+  //   map.panTo(position);
+  // }
 
   initMap();
 });
