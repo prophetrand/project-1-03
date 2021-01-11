408050c00ec49e35851f925d4d9560c9143f586e
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2021-01-11 00:48:50 -0800

    removed pet.js and finder.html because we are ditching that project and set up the last fm html and js files for testing on my end

---
+++ last_fm.js
@@ -0,0 +1,28 @@
+// Application name:	Top Tracks Geography
+// API key:	      6b9c504e4c3ebdbd387620fe00865255
+// Shared secret: 387c82747a3b71501f12df7c8b38aa4d
+// Registered to: gcvarela21
+
+var apiKey = '6b9c504e4c3ebdbd387620fe00865255';
+var url ='http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=6b9c504e4c3ebdbd387620fe00865255';
+
+console.log("your js file is active");
+
+function test() {
+			
+		console.log('test function has started');	
+		// Within $.ajax{...} is where we fill out our query 
+		$.ajax({
+			url: url,
+
+			// Here is where we handle the response we got back from Last FM
+			success: function(response) {
+				console.log(response);
+				console.log('I must be getting a response'); 
+			}
+		});
+	
+    }
+
+//  the test fucntion starts because I called it
+test();
