935b13569f5421b5dd21dd7c2b5ba7007390c1d7
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2021-01-11 22:23:10 -0800

    getting top 5 artists to apend to the html doc

--- last_fm.js
+++ last_fm.js
@@ -36,13 +36,28 @@ $(document).ready(function(){
 			var topName5 = response.topartists.artist[4].name;
 
 			var listeners1 = response.topartists.artist[0].listeners;
-			var url_1 = response.topartists.artist[0].url;
+			var listeners2 = response.topartists.artist[1].listeners;
+			var listeners3 = response.topartists.artist[2].listeners;
+			var listeners4 = response.topartists.artist[3].listeners;
+			var listeners5 = response.topartists.artist[4].listeners;
+
+			var urlFM1 = response.topartists.artist[0].url;
+			var urlFM2 = response.topartists.artist[1].url;
+			var urlFM3 = response.topartists.artist[2].url;
+			var urlFM4 = response.topartists.artist[3].url;
+			var urlFM5 = response.topartists.artist[4].url;
+			
+			//var url_1 = response.topartists.artist[0].url;
 			var img1 = response.topartists.artist[0].image[4]["#text"];
+			var img2 = response.topartists.artist[0].image[4]["#text"];
+			var img3 = response.topartists.artist[0].image[4]["#text"];
+			var img4 = response.topartists.artist[0].image[4]["#text"];
+			var img5 = response.topartists.artist[0].image[4]["#text"];
 
 			console.log(topName1);
 			//$("#dump-viewer").text("");
 			$("#dump-viewer").append(
-				"<div class='callout' style= 'margin-left: 20'; 'margin-right: 5vw' ><p>" + 'Artist Name: ' + topName1 + "</p><p>" + 'How Many Listeners: ' + listeners1 + "</p><img src=" + img1 +"/></div>"
+				"<div class='callout' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img1 +"/>","<a href='" + urlFM1 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a><p>" + 'Artist Name: ' + topName1 + "</p><p>" + 'How Many Listeners: ' + listeners1 + "</p></div>","<div class='callout' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img2 +"/>","<a href='" + urlFM2 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a><p>" + 'Artist Name: ' + topName2 + "</p><p>" + 'How Many Listeners: ' + listeners2 + "</p></div>","<div class='callout' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img3 +"/>","<a href='" + urlFM3 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a><p>" + 'Artist Name: ' + topName3 + "</p><p>" + 'How Many Listeners: ' + listeners3 + "</p></div>","<div class='callout' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img4 +"/>","<a href='" + urlFM4 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a><p>" + 'Artist Name: ' + topName4 + "</p><p>" + 'How Many Listeners: ' + listeners4 + "</p></div>", "<div class='callout' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img5 +"/>","<a href='" + urlFM5 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a><p>" + 'Artist Name: ' + topName5 + "</p><p>" + 'How Many Listeners: ' + listeners5 + "</p></div>"
 				)
 			}
 		    });
@@ -50,7 +65,3 @@ $(document).ready(function(){
 	});
 
 });
-
-
-//  the test fucntion starts because I called it
-//test();
