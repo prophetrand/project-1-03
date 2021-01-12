9f28616a5f76a824b5ad9a194f4d973f03abd56b
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2021-01-11 17:24:33 -0800

    i have the informations apenedign into the last FM html page

--- last_fm.js
+++ last_fm.js
@@ -4,19 +4,21 @@
 // Registered to: gcvarela21
 
 var apiKey = '6b9c504e4c3ebdbd387620fe00865255';
-var url ='http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists';
+//var url ='http://ws.audioscrobbler.com/2.0/?method=geo.getTopAlbums';
+var url ='http://ws.audioscrobbler.com/2.0/?method=geo.getTopartists';
 
 console.log("your js file is active");
 
-document.addEventListener('DOMContentLoaded', test);
-
-function test() {
-	document.getElementById('submitCountry').addEventListener('click', function(event){
+//document.addEventListener('DOMContentLoaded', test);
+// use AJax use JQuery call.. doc.ready wait for all the HTML to load before doing anything
+$(document).ready(function(){
+//function test() {
+	 $('#submitCountry').on('click', function(event){
 		event.preventDefault();
-		var country = document.getElementById('country').value; // this line gets the Country name from the form entry	
+		var country = $('#country').val().trim(); // this line gets the Country name from the form entry	
 		console.log('test function has started');	//// testign to see if function was ran
 		console.log(country);
-		// Within $.ajax{...} is where we fill out our query 
+		// Within $.ajax{document.getElementById...} is where we fill out our query 
 		$.ajax({
 			// ?method=geo.gettopartists&country=spain&api_key=YOUR_API_KEY&format=json
 			url: url + '&country=' + country + '&api_key=' + apiKey + '&limit=10&format=json',
@@ -25,27 +27,30 @@ function test() {
 			// Here is where we handle the response we got back from Last FM
 			success: function(response) {
 				console.log('Oh Sh!t there is a response! Look Down There:');
-				
-				//console.log(response);
-				//$("#dump-viewer").append(JSON.stringify(response.topartists.artist[0].name));
-				var topName1 = response.topartists.artist[0].name;
-				var topName2 = response.topartists.artist[1].name;
-				var topName3 = response.topartists.artist[2].name;
-				var topName4 = response.topartists.artist[3].name;
-				var topName5 = response.topartists.artist[4].name;
-				var topName6 = response.topartists.artist[5].name;
-				var topName7 = response.topartists.artist[6].name;
-				var topName8 = response.topartists.artist[7].name;
-				var topName9 = response.topartists.artist[8].name;
-				var topName10 = response.topartists.artist[9].name;
-				console.log(topName1);
+				console.log(response);
+
+			var topName1 = response.topartists.artist[0].name;
+			var topName2 = response.topartists.artist[1].name;
+			var topName3 = response.topartists.artist[2].name;
+			var topName4 = response.topartists.artist[3].name;
+			var topName5 = response.topartists.artist[4].name;
+
+			var listeners1 = response.topartists.artist[0].listeners;
+			var url_1 = response.topartists.artist[0].url;
+			var img1 = response.topartists.artist[0].image[4]["#text"];
+
+			console.log(topName1);
+			//$("#dump-viewer").text("");
+			$("#dump-viewer").append(
+				"<div class='callout' style= 'margin-left: 20'; 'margin-right: 5vw' ><p>" + 'Artist Name: ' + topName1 + "</p><p>" + 'How Many Listeners: ' + listeners1 + "</p><img src=" + img1 +"/></div>"
+				)
 			}
-			
-		});
+		    });
 	
 	});
 
-}
+});
+
 
 //  the test fucntion starts because I called it
 //test();
