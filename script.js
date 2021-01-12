5175e0ef79f7952be30cae650d538c07669bc643
Rand Hale <randluca@gmail.com>
2021-01-12 09:13:01 -0800

    progressively clean the Javascript

--- script.js
+++ script.js
@@ -50,16 +50,11 @@ $(document).ready(function(){
       $("#result-country").text("Results for: " + finder);
       $("#dump-viewer").empty();
       $.ajax({
-        // sample of what i want to string togeter:
-        // the url contains the last FM adress with the method of geo.topartists
-        // using srings to concatinate the rest of the search parameters, api key, and formsting to json data retrieval
         url: 'http://ws.audioscrobbler.com/2.0/?method=geo.getTopartists&country=' + finder + '&api_key=6b9c504e4c3ebdbd387620fe00865255&limit=10&format=json',
   
         // Here is where we handle the response we got back from Last FM
         success: function(response) {
-          // log to see if the function is running
-          console.log('Oh Sh!t there is a response! Look Down There:');
-          // dispaly all the info from the object retrieved fromt the call
+          // display all the info from the object retrieved fromt the call
           console.log(response);
           //// pointers to the top 5 artist names
           var topName1 = response.topartists.artist[0].name;
@@ -79,7 +74,7 @@ $(document).ready(function(){
           var urlFM3 = response.topartists.artist[2].url;
           var urlFM4 = response.topartists.artist[3].url;
           var urlFM5 = response.topartists.artist[4].url;
-          //// pointers to the top 5 artist provied images
+          //// pointers to the top 5 artist provided images
           var img1 = response.topartists.artist[0].image[4]["#text"];
           var img2 = response.topartists.artist[1].image[4]["#text"];
           var img3 = response.topartists.artist[2].image[4]["#text"];
@@ -89,7 +84,6 @@ $(document).ready(function(){
           // verifing that the information is retrieved correctly
           console.log(topName1);
           
-  
           /// appending a <div> <img /> <a></a> <p></p> <p></p> </div>
           // with the data from the the api call
           $("#dump-viewer").append(
