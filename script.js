1d1d957e603f3f827ea5b902493c499f211e4fa2
Rand Hale <randluca@gmail.com>
2021-01-12 08:55:03 -0800

    append results to page and properly empty result contents when a new search is performed

--- script.js
+++ script.js
@@ -47,7 +47,57 @@ $(document).ready(function(){
   // The event listener and function below CAN be edited. Currently serves as a placeholder and targets the button in the HTML with the ID of "search-go." Feel free to change this as well.
   $("#search-go").on("click", function(){
     if (marker != null) {
-      console.log(finder);
+      $("#result-country").text("Results for: " + finder);
+      $("#dump-viewer").empty();
+      $.ajax({
+        // sample of what i want to string togeter:
+        // the url contains the last FM adress with the method of geo.topartists
+        // using srings to concatinate the rest of the search parameters, api key, and formsting to json data retrieval
+        url: 'http://ws.audioscrobbler.com/2.0/?method=geo.getTopartists&country=' + finder + '&api_key=6b9c504e4c3ebdbd387620fe00865255&limit=10&format=json',
+  
+        // Here is where we handle the response we got back from Last FM
+        success: function(response) {
+          // log to see if the function is running
+          console.log('Oh Sh!t there is a response! Look Down There:');
+          // dispaly all the info from the object retrieved fromt the call
+          console.log(response);
+          //// pointers to the top 5 artist names
+          var topName1 = response.topartists.artist[0].name;
+          var topName2 = response.topartists.artist[1].name;
+          var topName3 = response.topartists.artist[2].name;
+          var topName4 = response.topartists.artist[3].name;
+          var topName5 = response.topartists.artist[4].name;
+          //// pointers to the top 5 artist listeners count
+          var listeners1 = response.topartists.artist[0].listeners;
+          var listeners2 = response.topartists.artist[1].listeners;
+          var listeners3 = response.topartists.artist[2].listeners;
+          var listeners4 = response.topartists.artist[3].listeners;
+          var listeners5 = response.topartists.artist[4].listeners;
+          //// pointers to the top 5 artist links to the lastfm web site
+          var urlFM1 = response.topartists.artist[0].url;
+          var urlFM2 = response.topartists.artist[1].url;
+          var urlFM3 = response.topartists.artist[2].url;
+          var urlFM4 = response.topartists.artist[3].url;
+          var urlFM5 = response.topartists.artist[4].url;
+          //// pointers to the top 5 artist provied images
+          var img1 = response.topartists.artist[0].image[4]["#text"];
+          var img2 = response.topartists.artist[1].image[4]["#text"];
+          var img3 = response.topartists.artist[2].image[4]["#text"];
+          var img4 = response.topartists.artist[3].image[4]["#text"];
+          var img5 = response.topartists.artist[4].image[4]["#text"];
+  
+          // verifing that the information is retrieved correctly
+          console.log(topName1);
+          
+  
+          /// appending a <div> <img /> <a></a> <p></p> <p></p> </div>
+          // with the data from the the api call
+          $("#dump-viewer").append(
+          "<div class='callout large primary' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img1 +"/>","<a href='" + urlFM1 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a>","<p>" + 'Artist Name: ' + topName1 + "</p>","<p>" + 'How Many Listeners: ' + listeners1 + "</p></div>","<div class='callout large primary' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img2 +"/>","<a href='" + urlFM2 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a>","<p>" + 'Artist Name: ' + topName2 + "</p>","<p>" + 'How Many Listeners: ' + listeners2 + "</p></div>","<div class='callout large primary' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img3 +"/>","<a href='" + urlFM3 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a>","<p>" + 'Artist Name: ' + topName3 + "</p>","<p>" + 'How Many Listeners: ' + listeners3 + "</p></div>","<div class='callout large primary' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img4 +"/>","<a href='" + urlFM4 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a>","<p>" + 'Artist Name: ' + topName4 + "</p>","<p>" + 'How Many Listeners: ' + listeners4 + "</p></div>", "<div class='callout large primary' style= 'margin-left: 20'; 'margin-right: 5vw' ><img src=" + img5 +"/>","<a href='" + urlFM5 + "' target='_blank'>" + 'Check Out the Artist on Last FM!' + "</a>","<p>" + 'Artist Name: ' + topName5 + "</p>","<p>" + 'How Many Listeners: ' + listeners5 + "</p></div>"
+          )
+        }
+      });
+
     } else {
       // Append text to page that says a location has to be chosen.
       $("#place-check").text("Please choose a country on the map or in the search box.");
